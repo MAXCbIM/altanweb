43146a59cec7878af73fb3fe202ab622402d2dc6
MAXCbIM <argunovmaximmaxim@gmail.com>
2021-12-23 21:02:19 +0900

    сделали регу

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,8 +6,30 @@ app = Flask(__name__)
 
 @app.route("/")
 def index_page():
-    return render_template("index.html")
+	return render_template("home.html")
+
+@app.route("/product", methods=["GET", "POST"])
+def product_page():
+	msg = ""
+	name = ""
+	pwd = ""
+	if request.method == "POST":
+		name = request.form.get("name")
+		pwd = request.form.get("pwd")
+
+		msg = f"{name} {pwd}"
+
+	return render_template("product.html", msg=msg, n=name, p=pwd)
+
+# http://127.0.0.1:5000/test?v1=hello&v2=1234
+@app.route("/test")
+def test_page():
+	var_1 = request.args.get("v1")
+	return f"""
+	<h3>Test</h3>
+	<p> Var 1: {var_1} </p>
+	"""
 
 # точка входа
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
